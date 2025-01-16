6b3950c16c33fe835ef236c3c2f50445183d1716
Mostafa Mohamed <mostaamohamedd985@gmail.com>
2025-01-17 00:41:35 +0200

    adding segment tree lazy propagation

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,44 +1,77 @@
-struct node {
-    int mn, mx, cnt;
-    node() : mn(1e9), mx(-1e9), cnt(0) {}
-    node(int x) : mn(x), mx(x), cnt(1) {}
-    node(int mn, int mx, int cnt) : mn(mn), mx(mx), cnt(cnt) {}
-};
-struct segment_tree {
-    vector<int> a;
-    vector<node> seg;
-    segment_tree(const vector<int> v) : a(v) {
-        seg.resize(4 * sz(a));
+template<typename T>
+class segment_tree { //1-based
+#define LEFT (idx << 1)
+#define RIGHT (idx << 1 | 1)
+#define MID ((start + end) >> 1)
+    int n;
+    vector<T> tree, lazy;
+    T merge(const T& left, const T& right) {}
+    void pushdown(int idx, int start, int end) {
+        if (lazy[idx] == 0)
+            return;
+        // update tree[idx] with lazy[idx]
+        tree[idx] += lazy[idx];
+        if (start != end) {
+            lazy[LEFT] += lazy[idx];
+            lazy[RIGHT] += lazy[idx];
+        }
+        lazy[idx] = 0; //clear lazy
     }
-    node merge(node l, node r) {
-        return {min(l.mn, r.mn), max(l.mx, r.mx), l.cnt + r.cnt};
+    void build(int idx, int start, int end) {
+        if (start == end)
+            return;
+        build(LEFT, start, MID);
+        build(RIGHT, MID + 1, end);
+        tree[idx] = merge(tree[LEFT], tree[RIGHT]);
     }
-    void build(int i, int s, int e) {
-        if (s == e) {
-            seg[i] = node(a[s]);
+    void build(int idx, int start, int end, const vector<T>& arr) {
+        if (start == end) {
+            tree[idx] = arr[start];
             return;
         }
-        build(i * 2, s, (s + e) / 2);
-        build(i * 2 + 1, (s + e) / 2 + 1, e);
-        seg[i] = merge(seg[i * 2], seg[i * 2 + 1]);
+        build(LEFT, start, MID, arr);
+        build(RIGHT, MID + 1, end, arr);
+        tree[idx] = merge(tree[LEFT], tree[RIGHT]);
     }
-    node query(int i, int s, int e, int l, int r, int k) {
-        if (e < l || s > r || k >= seg[i].mx)
-            return {};
-        if (s >= l && e <= r && k < seg[i].mn)
-            return seg[i];
-        return merge(query(i * 2, s, (s + e) / 2, l, r, k), query(i * 2 + 1, (s + e) / 2 + 1, e, l, r, k));
+    T query(int idx, int start, int end, int from, int to) {
+        pushdown(idx, start, end);
+        if (from <= start && end <= to)
+            return tree[idx];
+        if (to <= MID)
+            return query(LEFT, start, MID, from, to);
+        if (MID < from)
+            return query(RIGHT, MID + 1, end, from, to);
+        return merge(query(LEFT, start, MID, from, to),
+                     query(RIGHT, MID + 1, end, from, to));
     }
-    void update(int i, int s, int e, int idx, int val) {
-        if (idx < s || idx > e)
+    void update(int idx, int start, int end, int l, int r, const T& val) {
+        pushdown(idx, start, end);
+        if (r < start || end < l)
             return;
-        if (idx == s && s == e) {
-            a[s] = val;
-            seg[i] = node(a[s]);
+        if (l <= start && end <= r) {
+            lazy[idx] += val; //update lazy
+            pushdown(idx, start, end);
             return;
         }
-        update(i * 2, s, (s + e) / 2, idx, val);
-        update(i * 2 + 1, (s + e) / 2 + 1, e, idx, val);
-        seg[i] = merge(seg[i * 2], seg[i * 2 + 1]);
+        update(LEFT, start, MID, l, r, val);
+        update(RIGHT, MID + 1, end, l, r, val);
+        tree[idx] = merge(tree[LEFT], tree[RIGHT]);
     }
+public:
+    segment_tree(int n) : n(n), tree(n << 2), lazy(n << 2) {}
+    segment_tree(const vector<T>& v) {
+        n = v.size() - 1;
+        tree = vector<T>(n << 2);
+        lazy = vector<T>(n << 2);
+        build(1, 1, n, v);
+    }
+    T query(int l, int r) {
+        return query(1, 1, n, l, r);
+    }
+    void update(int l, int r, const T& val) {
+        update(1, 1, n, l, r, val);
+    }
+#undef LEFT
+#undef RIGHT
+#undef MID
 };
