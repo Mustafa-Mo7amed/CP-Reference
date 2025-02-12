78e5ca7640f58ae49485265271782b25309bcc3a
Mostafa Mohamed <mostaamohamedd985@gmail.com>
2025-02-12 15:14:04 +0200

    adding bridges and handling it for undirected graphs

--- Graph/tarjan.cpp
+++ Graph/tarjan.cpp
@@ -2,24 +2,29 @@ vector<vector<int>> components; // stores all strongly connected components
 // dfs_num: used as a visited array and it represents the dfs_number of nodes
 // lowLink: represents the lowest dfs_number the current node can reach (ancestors would have lower dfs_number)
 // compId: its indices represents nodes and the value represents the index of the component that this node belongs to in "components" vector
-vector<int> dfs_num(n + 1, -1), low(n + 1), compId(n + 1);
+vector<int> dfs_num(n + 1, -1), lowLink(n + 1), compId(n + 1);
 vector<bool> inStack(n + 1); // marks the current path in dfs to detect cycles
+set<pair<int, int>> bridges;
 stack<int> stk; // stores the nodes of the current connected component
 int dfsn = 0; // marks each node with a dfs_number
-function<void(int)> tarjan = [&](int node) {
-    dfs_num[node] = low[node] = dfsn++;
+function<void(int, int)> tarjan = [&](int node, int par) {
+    dfs_num[node] = lowLink[node] = dfsn++;
     inStack[node] = true;
     stk.emplace(node);
     for (auto& i : adj[node]) {
+        if (i == par)
+            continue;
         if (dfs_num[i] == -1) {
-            tarjan(i);
-            low[node] = min(low[node], low[i]);
+            tarjan(i, node);
+            lowLink[node] = min(lowLink[node], lowLink[i]);
         }
         else
-            low[node] = min(low[node], dfs_num[i]);
+            lowLink[node] = min(lowLink[node], dfs_num[i]);
     }
     inStack[node] = false;
-    if (low[node] == dfs_num[node]) {
+    if (lowLink[node] == dfs_num[node]) {
+        if (par != -1)
+            bridges.emplace(par, node); 
         int x = -1;
         components.emplace_back(); // pushing an empty vector
         while (x != node) { // till the root
